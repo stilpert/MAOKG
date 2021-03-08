7c47239f4f598cd95a616f8726917a5f824cb517
stilpert <stilpert@gmail.com>
2021-03-08 19:13:22 +0200

    lab 2 comlete

--- lab2/src/FirstTask.java
+++ lab2/src/FirstTask.java
@@ -12,9 +12,12 @@ public class FirstTask extends JPanel implements ActionListener {
     private double scale = 1;
     private double delta = 0.01;
     private double dx = 1;
-    private double tx = 200;
+    private double tx = 300;
     private double dy = 1;
     private double ty = 200;
+    private double edge = 400;
+    private double sx = 300;
+    private double sy = 200;
 
     public FirstTask() {
         timer = new Timer(10, this);
@@ -39,30 +42,28 @@ public class FirstTask extends JPanel implements ActionListener {
                 RenderingHints.VALUE_RENDER_QUALITY);
 
         g2d.translate(tx, ty);
-
         g2d.scale(scale, scale);
 
         g2d.setBackground(new Color(139, 0, 0));
-        g2d.clearRect(50, 50, 500, 300);
+        g2d.clearRect(-250, -150, 500, 300);
 
         g2d.setColor(Color.YELLOW);
         g2d.setStroke(new BasicStroke(10));
-        g2d.drawLine(50, 150, 550, 150);
-        g2d.drawLine(50, 250, 550, 250);
-        g2d.drawLine(300, 50, 300, 150);
-        g2d.drawLine(300, 250, 300, 350);
-        g2d.drawLine(200, 150, 200, 250);
-        g2d.drawLine(400, 150, 400, 250);
+        g2d.drawLine(-250, 50, 250, 50);
+        g2d.drawLine(-250, -50, 250, -50);
+        g2d.drawLine(-100, -50, -100, 50);
+        g2d.drawLine(100, -50, 100, 50);
+        g2d.drawLine(0, 50, 0, 150);
+        g2d.drawLine(0, -50, 0, -150);
 
         GradientPaint gp = new GradientPaint(0, 0,
-                Color.PINK, 200, 200, Color.ORANGE, false);
+                Color.PINK, 50, 50, Color.ORANGE, true);
         g2d.setPaint(gp);
 
         double points[][] = {
-                {0, 0}, {90, 15}, {180, 0}, {180, 80}, {90, 65}, {0 , 80}, {0,0}
+                {-90, -40}, {0, -25}, {90, -40}, {90, 40}, {0, 25}, {-90 , 40}, {-90, -40}
         };
         GeneralPath cRect = new GeneralPath();
-        g2d.translate(210, 160);
         cRect.moveTo(points[0][0], points[0][1]);
         for (int i = 1; i < points.length; i++)
             cRect.lineTo(points[i][0], points[i][1]);
@@ -85,24 +86,24 @@ public class FirstTask extends JPanel implements ActionListener {
 
     }
     public void actionPerformed(ActionEvent e) {
-        if ( scale < 0.01 ) {
+        if ( scale < 0.1 ) {
             delta = -delta;
         } else if (scale > 0.99) {
             delta = -delta;
         }
-//        if ( tx < -maxWidth/3 ) {
-//            dx = -dx;
-//        } else if ( tx > maxWidth/3 ) {
-//            dx = -dx;
-//        }
-//        if ( ty < -maxHeight/3 ) {
-//            dy = -dy;
-//        } else if ( ty > maxHeight/3 ) {
-//            dy = -dy;
-//        }
+
+        if ( tx <= sx && ty < sy+edge) {
+            ty += dy;
+        } else if (ty >= sy+edge && tx < sx+edge) {
+            tx += dx;
+        } else if (ty > sy && tx >= sx+edge){
+            ty -= dy;
+        } else if (tx > sx && ty >= sy) {
+            tx -= dx;
+        }
+
         scale += delta;
-//        tx +=         dx;
-//        ty +=         dy;
+
         repaint();
     }
 }
