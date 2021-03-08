5f070b2a0e6b1c41bbaf771867a69678e2cf2efd
stilpert <stilpert@gmail.com>
2021-03-08 18:15:24 +0200

    add scaling

---
+++ lab2/src/FirstTask.java
@@ -0,0 +1,108 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.GeneralPath;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class FirstTask extends JPanel implements ActionListener {
+    private static int maxWidth;
+    private static int maxHeight;
+    Timer timer;
+    private double scale = 1;
+    private double delta = 0.01;
+    private double dx = 1;
+    private double tx = 200;
+    private double dy = 1;
+    private double ty = 200;
+
+    public FirstTask() {
+        timer = new Timer(10, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.setBackground(new Color(128, 128, 0));
+        g2d.clearRect(0, 0, maxWidth, maxHeight);
+        // малюємо рамку
+        g2d.setColor(Color.ORANGE);
+        BasicStroke bs = new BasicStroke(12, BasicStroke.CAP_ROUND,
+                BasicStroke.JOIN_BEVEL);
+        g2d.setStroke(bs);
+        g2d.drawRect(20, 20, 960, 760);
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+
+        g2d.translate(tx, ty);
+
+        g2d.scale(scale, scale);
+
+        g2d.setBackground(new Color(139, 0, 0));
+        g2d.clearRect(50, 50, 500, 300);
+
+        g2d.setColor(Color.YELLOW);
+        g2d.setStroke(new BasicStroke(10));
+        g2d.drawLine(50, 150, 550, 150);
+        g2d.drawLine(50, 250, 550, 250);
+        g2d.drawLine(300, 50, 300, 150);
+        g2d.drawLine(300, 250, 300, 350);
+        g2d.drawLine(200, 150, 200, 250);
+        g2d.drawLine(400, 150, 400, 250);
+
+        GradientPaint gp = new GradientPaint(0, 0,
+                Color.PINK, 200, 200, Color.ORANGE, false);
+        g2d.setPaint(gp);
+
+        double points[][] = {
+                {0, 0}, {90, 15}, {180, 0}, {180, 80}, {90, 65}, {0 , 80}, {0,0}
+        };
+        GeneralPath cRect = new GeneralPath();
+        g2d.translate(210, 160);
+        cRect.moveTo(points[0][0], points[0][1]);
+        for (int i = 1; i < points.length; i++)
+            cRect.lineTo(points[i][0], points[i][1]);
+        cRect.closePath();
+        g2d.fill(cRect);
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("First task");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 850);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.add(new FirstTask());
+        frame.setVisible(true);
+
+        Dimension size = frame.getSize();
+        Insets insets = frame.getInsets();
+        maxWidth = size.width - insets.left - insets.right;
+        maxHeight = size.height - insets.top - insets.bottom;
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        if ( scale < 0.01 ) {
+            delta = -delta;
+        } else if (scale > 0.99) {
+            delta = -delta;
+        }
+//        if ( tx < -maxWidth/3 ) {
+//            dx = -dx;
+//        } else if ( tx > maxWidth/3 ) {
+//            dx = -dx;
+//        }
+//        if ( ty < -maxHeight/3 ) {
+//            dy = -dy;
+//        } else if ( ty > maxHeight/3 ) {
+//            dy = -dy;
+//        }
+        scale += delta;
+//        tx +=         dx;
+//        ty +=         dy;
+        repaint();
+    }
+}
